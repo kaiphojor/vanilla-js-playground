0c2ee81cb68c9a888b09562541b8991d70a10986
Bluemage <jihoopark7666@gmail.com>
2025-03-18 11:29:56 +0900

    ADD 250318 files

---
+++ chapter05/test238_main.js
@@ -0,0 +1,8 @@
+// main.js
+import { add, subtract } from './test238_math.js';
+
+const result1 = add(5, 3);
+console.log(`5 + 3 = ${result1}`);
+
+const result2 = subtract(9, 4);
+console.log(`9 - 4 = ${result2}`);
